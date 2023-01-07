e8bdf31af72e08e52c542d3f7c041da0c6b00a96
Sebas1236 <cptsebas@gmail.com>
2023-01-07 17:14:46 -0600

    Renderizar usuarios e interfaz más bonita

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -16,7 +16,9 @@ socket.on('connect', function() {
     console.log('Conectado al servidor');
 
     socket.emit('entrarChat', usuario, function( resp ){
-        console.log('Usuarios conectados', resp);
+        // console.log('Usuarios conectados', resp);
+        //La respuesta es un arreglo de usuarios
+        renderizarUsuarios(resp);
     });
 });
 
@@ -45,8 +47,7 @@ socket.on('crearMensaje', function(mensaje) {
 
 //*Escuchar cambios de usuarios. Cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-
-    console.log(personas);
+    renderizarUsuarios(personas);
 });
 
 //* Mensajes privados
